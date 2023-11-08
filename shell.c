b3728750980f0ca5891e1759372ab0fab4967147
Anass El Omari <anas.iramol@gmail.com>
2023-11-08 12:35:33 -0800

    reducing files

--- shell.c
+++ shell.c
@@ -1,6 +1,127 @@
 #include "shell.h"
 
 /**
+  * line_interpreter - wait for user to enter command and read line
+  * Return: line
+  */
+
+char *line_interpreter(void)
+{
+	size_t length = 0;
+	ssize_t i;
+	char *l = NULL;
+
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "$ ", 2);
+	i = getline(&l, &length, stdin);
+	if (i == -1)
+	{
+		free(l);
+		return (NULL);
+	}
+	return (l);
+}
+
+/**
+  * line_handler - handle command by command
+  * @l: line to be handled
+  * Return: command
+  */
+
+char **line_handler(char *l)
+{
+	char *handler = NULL, *mem = NULL, **cmd = NULL, sep[] = " \t\n";
+	int i = 0, c = 0;
+
+	if (!l)
+		return (NULL);
+	mem = _strdup(l);
+	handler = strtok(mem, sep);
+	if (handler == NULL)
+	{
+		free(l);
+		l = NULL;
+		free(mem);
+		mem = NULL;
+	}
+	while (handler)
+	{
+		c++;
+		handler = strtok(NULL, sep);
+	}
+	free(mem);
+	mem = NULL;
+	cmd = malloc(sizeof(char *) * (c + 1));
+	if (!cmd)
+	{
+		free(l);
+		l = NULL;
+		return (NULL);
+	}
+	handler = strtok(l, sep);
+	while (handler)
+	{
+		cmd[i] = _strdup(handler);
+		handler = strtok(NULL, sep);
+		i++;
+	}
+	free(l);
+	l = NULL;
+	cmd[i] = NULL;
+	return (cmd);
+}
+
+/**
+  * array_clearer - free the array
+  * @a: two dimentionnel array
+  */
+
+void array_clearer(char **a)
+{
+	int i;
+
+	if (!a)
+		return;
+	for (i = 0; a[i]; i++)
+	{
+		free(a[i]);
+		a[i] = NULL;
+	}
+	free(a);
+	a = NULL;
+}
+
+/**
+  * shell_exe - exexute command
+  * @cmd: command to be executed
+  * @var: variable
+  * Return: status 0 || 127
+  */
+
+int shell_exe(char **var, char **cmd)
+{
+	int stat;
+	pid_t c;
+
+	c = fork();
+	if (c == 0)
+	{
+		if (execve(cmd[0], cmd, environ) == -1)
+		{
+			perror(var[0]);
+			array_clearer(cmd);
+			exit(127);
+		}
+	}
+	else
+	{
+		waitpid(c, &stat, 0);
+		array_clearer(cmd);
+	}
+	return (WEXITSTATUS(stat));
+}
+
+/**
   * main - shell main program
   * @var: variable for error
   * @i: unused variable
---
+++ utils.c
@@ -0,0 +1,117 @@
+#include "shell.h"
+
+/**
+  * _strcat - concatenates two strings
+  * @dest: string to append to
+  * @src: string
+  * Return: a pointer to the resulting string
+  */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+  * _strcmp - compares two strings
+  * @s1: 1st string
+  * @s2: 2nd string
+  * Return: 0 || s1 - s2
+  */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+/**
+  * *_strcpy - copy the string pointed to by src
+  * including the terminating null byte (\0)
+  * to the buffer pointed to by dest
+  * @dest: pointer to the buffer in which we copy the string
+  * @src: string to be copied
+  * Return: the pointer to dest
+  */
+
+char *_strcpy(char *dest, char *src)
+{
+	int l, i;
+
+	l = 0;
+
+	while (src[l] != '\0')
+	{
+		l++;
+	}
+
+	for (i = 0; i < l; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+  * _strdup - duplicate string
+  * @s: charcter
+  * Return: pointer
+  */
+
+char *_strdup(char *s)
+{
+	char *p;
+	int length;
+	int i;
+
+	if (s == NULL)
+	return (NULL);
+	length = _strlen(s);
+	p = malloc(sizeof(char) * (length + 1));
+	if (!p)
+		return (NULL);
+	for (i = 0; *s != '\0'; s++, i++)
+		p[i] = s[0];
+	p[i++] = '\0';
+	return (p);
+}
+
+
+/**
+  * _strlen - returns the length of a string
+  * @s: the string
+  * Return: returns the lenght
+  */
+
+int _strlen(char *s)
+{
+	int n, i;
+
+	i = 0;
+	for (n = 0; s[n] != '\0'; n++)
+	i++;
+	return (i);
+}
