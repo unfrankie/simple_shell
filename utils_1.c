f7a85cb7dbae1874a55f3a1a2c89246e2978bf49
unfrankie <141495574+unfrankie@users.noreply.github.com>
2023-11-09 19:56:23 +0100

    Update utils_1.c

--- utils_1.c
+++ utils_1.c
@@ -19,3 +19,52 @@ void array_clearer(char **a)
 	free(a);
 	a = NULL;
 }
+
+/**
+  * _itoa - convert an integer to string
+  * @n: the integer
+  * Return: string
+  */
+
+char *_itoa(int n)
+{
+	char max[20];
+	int i = 0;
+
+	if (n == 0)
+		max[i++] = '0';
+	else
+	{
+		while (n > 0)
+		{
+			max[i++] = (n % 10) + '0';
+			n /= 10;
+		}
+	}
+	max[i] = '\0';
+	reverse_string(max, i);
+	return (_strdup(max));
+}
+
+/**
+  * reverse_string - reverse string
+  * @str: string to be reversed
+  * @len: the length of the string
+  * Return: (void).
+  */
+  
+void reverse_string(char *str, int len)
+{
+	char temp;
+	int s = 0;
+	int e = len - 1;
+
+	while (s < e)
+	{
+		temp = str[s];
+		str[s] = str[e];
+		str[e] = temp;
+		s++;
+		e--;
+	}
+}
