a5ebffec74fdb8f69ad32c53dc0c74eb2667fa22
unfrankie <141495574+unfrankie@users.noreply.github.com>
2023-11-09 19:48:43 +0100

    Update shell.c

--- shell.c
+++ shell.c
@@ -72,26 +72,6 @@ char **line_handler(char *l)
 }
 
 /**
-  * array_clearer - free the array
-  * @a: two dimentionnel array
-  */
-
-void array_clearer(char **a)
-{
-	int i;
-
-	if (!a)
-		return;
-	for (i = 0; a[i]; i++)
-	{
-		free(a[i]);
-		a[i] = NULL;
-	}
-	free(a);
-	a = NULL;
-}
-
-/**
   * shell_exe - exexute command
   * @cmd: command to be executed
   * @var: variable
