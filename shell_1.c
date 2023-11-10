2b71d41054deb4a17f0b85f544bcbae829d803b3
unfrankie <141495574+unfrankie@users.noreply.github.com>
2023-11-10 19:14:42 +0100

    Update shell_1.c

--- shell_1.c
+++ shell_1.c
@@ -1,6 +1,60 @@
 #include "shell.h"
 
 /**
+  * add_node_end - add a new node at the end
+  * @head: head pointer
+  * @str: string
+  * Return: the address of the new element || NULL
+  */
+
+directory *add_node_end(directory **head, const char *str)
+{
+	directory *l;
+	int i = 0;
+	directory *l1 = *head;
+
+	while (str[i])
+		i++;
+	l = malloc(sizeof(list_t));
+	if (!l)
+	{
+		return (NULL);
+	}
+	l->str = strdup(str);
+	l->len = i;
+	l->next = NULL;
+	if (*head == NULL)
+	{
+		*head = l;
+		return (l);
+	}
+	while (l1->next)
+		l1 = l1->next;
+
+	l1->next = l;
+
+	return (l);
+}
+
+/**
+  * free_list - free list
+  * @head: head pointer
+  */
+
+void free_list(directory *head)
+{
+	directory *l;
+
+	while (head)
+	{
+		l = head->next;
+		free(head->str);
+		free(head);
+		head = l;
+	}
+}
+
+/**
   * main - shell main program
   * @var: variable
   * Return: status
