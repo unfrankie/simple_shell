9638d9a93ca50ce32994652a77ede57747b1dd4b
unfrankie <141495574+unfrankie@users.noreply.github.com>
2023-11-12 18:46:59 +0100

    redecing code and files, also fixing shell 0.3

--- shell_1.c
+++ shell_1.c
@@ -1,82 +1,6 @@
 #include "shell.h"
 
 /**
-  * add_node_end - add a new node at the end
-  * @head: head pointer
-  * @str: string
-  * Return: the address of the new element || NULL
-  */
-
-list_directory *add_node_end(list_directory **head, const char *str)
-{
-	list_directory *new, *mem;
-
-	if (head == NULL || str == NULL)
-		return (NULL);
-	new = malloc(sizeof(list_directory));
-	if (new == NULL)
-		return (NULL);
-	new->d = _strdup(str);
-	if (new->d == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-	new->next = NULL;
-	if (*head == NULL)
-	{
-		*head = new;
-		return (*head);
-	}
-	mem = *head;
-	while (mem->next != NULL)
-		mem = mem->next;
-	mem->next = new;
-	return (*head);
-}
-
-/**
-  * free_list - free list
-  * @head: head pointer
-  */
-
-void free_list(list_directory *head)
-{
-	list_directory *l;
-
-	while (head)
-	{
-		l = head->next;
-		free(head->d);
-		free(head);
-		head = l;
-	}
-}
-
-/**
-  * dir_builder - build a linked list of directory path
-  * Return: linked list of directory path
-  */
-
-list_directory *dir_builder(void)
-{
-	list_directory *head = NULL;
-	char *path, *func;
-
-	path = env_summoner("INITIATOR");
-	if (!path)
-		return (NULL);
-	func = strtok(path, ":");
-	while (func)
-	{
-		add_node_end(&head, func);
-		func = strtok(NULL, ":");
-	}
-	free(path);
-	return (head);
-}
-
-/**
   * builtin_1 - builtin checker
   * @cmd: command to be checked
   * Return: 1 true || 0 false
@@ -112,4 +36,69 @@ void builtinner(char **bicmd, char **var, int *exit_v, int index)
 		exit_func(bicmd, exit_v);
 	else if (_strcmp(bicmd[0], "env") == 0)
 		env_func(bicmd, exit_v);
+}
+
+/**
+  * exit_func - exit shell function
+  * @cmd: command
+  * @exit_v: exit value
+  */
+
+void exit_func(char **cmd, int *exit_v)
+{
+	array_clearer(cmd);
+	exit(*exit_v);
+}
+
+/**
+  * env_func - display environement
+  * @cmd: command
+  * @exit_v: exit value
+  */
+
+void env_func(char **cmd, int *exit_v)
+{
+	int i;
+
+	for (i = 0; environ[i]; i++)
+	{
+		write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
+		write(STDOUT_FILENO, "\n", 1);
+	}
+	(*exit_v) = 0;
+	array_clearer(cmd);
+}
+
+/**
+  * main - shell main program
+  * @var: variable
+  * @i: unused integer
+  * Return: status
+  */
+
+int main(int i, char **var)
+{
+	char **cmd = NULL, *l = NULL, *trail = NULL;
+	int st = 0, index = 0;
+	(void) i;
+
+	while (1)
+	{
+		l = line_interpreter();
+		if (l == NULL)
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			free(trail);
+			return (st);
+		}
+		cmd = line_handler(l);
+		if (!cmd)
+			continue;
+		if (builtin_1(cmd[0]))
+			builtinner(cmd, var, &st, index);
+		else
+			st = shell_exe(var, cmd, index);
+	}
+	return (0);
 }
